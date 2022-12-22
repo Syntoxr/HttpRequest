1e4f65507fe0ba0b26538540652f5bb7df32ea93
Syntox <63484271+Syntoxr@users.noreply.github.com>
2022-12-22 08:43:47 +0100

    initial plugin

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("java")
+    id("xyz.jpenilla.run-paper") version "2.0.0"
+}
+
+group = "io.github.syntoxr.httprequest"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(18))
+}
+
+tasks {
+    runServer {
+        // Configure the Minecraft version for our task.
+        // This is the only required configuration besides applying the plugin.
+        // Your plugin's jar (or shadowJar if present) will be used automatically.
+        minecraftVersion("1.19.3")
+    }
+}
+
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
